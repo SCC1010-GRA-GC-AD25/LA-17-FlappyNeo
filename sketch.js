ebe03b41de1da16a7a1b033dc340075ec075275c
Alyson Lapizco <160778240+alylaan@users.noreply.github.com>
2025-12-02 04:23:45 +0000

    listo

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
-let imagenFondo
-let imagenInicio
-let personaje
+let fondoJuego
+let fondoInicio
 let pared
+let jaeminBird
 let x = 0
 let posX = 100
 let posY = 100
@@ -18,103 +18,132 @@ let sonido = true
 
 
 function preload() {
-  // put preload code here
-  fondojuego = loadImage("./images/fondojuego00.png")
-  fondoinicio = loadImage("./images/fondoinicio00.jpg")
+  // Cargar imágenes con los nombres correctos
+  fondoJuego = loadImage("./images/fondoJuego.jpg")
+  fondoInicio = loadImage("./images/fondoInicio.jpg")
   pared = loadImage("./images/pared.png")
+  jaeminBird = loadImage("./images/jaeminBird.png")
+  
+  // Cargar sonidos
   musicaRecord = loadSound("./sounds/aplauso.wav")
-  musicaFondo = loadSound("./sounds/musicafondo.mp3")
+  musicaFondo = loadSound("./sounds/musicaFondo.mp3")
 }
 
 function setup() {
-  // put setup code here
-  createCanvas(1000,512)
+  createCanvas(1000, 512)
   noCursor()
 }
 
 function draw() {
-  // put drawing code here
   if (estado === 1) {
+    // JUEGO EN MARCHA
     background(255)
     imageMode(CORNER)
-    image(imagenFondo,x,0)
-    image(imagenFondo,x+imagenFondo.width,0)
+    
+    // Fondo en movimiento
+    image(fondoJuego, x, 0)
+    image(fondoJuego, x + fondoJuego.width, 0)
     x = x - 5
     dY = dY + 1
     posY = posY + dY
 
-    if (x <= -imagenFondo.width) {
+    if (x <= -fondoJuego.width) {
       x = 0
     }
 
-    //obstaculos
-    for (let i=0; i < wallX.length; i++){
+    // Obstáculos (paredes)
+    for (let i = 0; i < wallX.length; i++) {
       imageMode(CENTER)
-      image(pared,wallX[i], wallY[i]-500)
-      image(pared,wallX[i], wallY[i]+500)
-      //Nuevos obstáculos
+      image(pared, wallX[i], wallY[i] - 500)
+      image(pared, wallX[i], wallY[i] + 500)
+      
+      // Nuevos obstáculos
       if (wallX[i] < -pared.width) {
         wallX[i] = width + 100
-        wallY[i] = random(200,300)
+        wallY[i] = random(200, 300)
       }
-      //Puntaje
+      
+      // Puntaje
       if (wallX[i] === posX) {
-        puntaje = puntaje  + 1
+        puntaje = puntaje + 1
         puntajeMax = max(puntajeMax, puntaje)
       }
 
-
       wallX[i] = wallX[i] - 5
-      //validar colisiones
-      if (posY < -60 || posY > height+60 ) {
+      
+      // Validar colisiones
+      if (posY < -60 || posY > height + 60) {
         musicaFondo.stop()
         estado = 0
       }
     }
 
-
-    //Despliega personaje
-    image(personaje,posX,posY,60,60)
-    text("Puntaje: " + puntaje, width/2 - 60,30)
-  } else if (estado === 0) {
-    background(0)
+    // Despliega personaje (Jaemin Bird)
+    imageMode(CENTER)
+    image(jaeminBird, posX, posY, 80, 80)
+    
+    // Mostrar puntaje
     fill(255)
-    cursor()
+    stroke(0)
+    strokeWeight(4)
+    textSize(32)
+    textAlign(CENTER)
+    text("Puntaje: " + puntaje, width / 2, 40)
+    
+  } else if (estado === 0) {
+    // PANTALLA DE INICIO / GAME OVER
     imageMode(CORNER)
-    image(imagenInicio,0,0,450,600)
-    textSize(24)
-    text("Puntaje Máximo: " + puntajeMax, 600, 100)
-    text("Clic para jugar",600,200)
+    image(fondoInicio, 0, 0, width, height)
+    
+    cursor()
+    
+    // Textos en la parte derecha (lado negro)
+    textAlign(CENTER)
+    
+    // Puntaje máximo (siempre visible)
+    textSize(40)
+    fill(255)
+    stroke(0)
+    strokeWeight(3)
+    text("Puntaje Máximo: " + puntajeMax, width * 0.75, 110)
+    
+    // Instrucción para jugar
+    textSize(32)
+    text("Presiona para iniciar", width * 0.75, 240)
+    
+    // Reproducir sonido de record
     if (puntajeMax > recordAnterior) {
       if (!musicaRecord.isPlaying() && sonido) {
-        musicaRecord.play(duration=2)
+        musicaRecord.play()
         sonido = false
       }
-    } 
-
+    }
   }
 }
 
 function mousePressed() {
   if (estado === 0) {
+    // Iniciar juego
     estado = 1
     x = 0
-    posX =100
+    posX = 100
     posY = 100
     dY = 3
     wallX = [500, 800, 1100]
-    wallY[0] = random(200,300)
-    wallY[1] = random(200,300)
-    wallY[2] = random(200,300)
+    wallY[0] = random(200, 300)
+    wallY[1] = random(200, 300)
+    wallY[2] = random(200, 300)
     puntaje = 0
     sonido = true
     recordAnterior = puntajeMax
+    
     if (musicaRecord.isPlaying()) {
       musicaRecord.stop()
     }
     musicaFondo.loop()
-    
     noCursor()
   }
+  
+  // Hacer que el pájaro salte
   dY = -15
 }
