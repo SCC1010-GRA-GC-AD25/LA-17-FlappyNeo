a64e4d843035ca314e1fa0f568f0dc7788a85da6
Alyson Lapizco <160778240+alylaan@users.noreply.github.com>
2025-12-02 06:58:10 +0000

    colision2.0

--- sketch.js
+++ sketch.js
@@ -1,4 +1,39 @@
-// Agrega esta función después de la función setup()
+let fondoJuego
+let fondoInicio
+let pared
+let jaeminBird
+let x = 0
+let posX = 100
+let posY = 100
+let dY = 3
+let estado = 0 // 0= menú; 1 = Juego
+let wallX = []
+let wallY = []
+let puntaje = 0
+let puntajeMax = 0
+let recordAnterior = 0
+let musicaRecord
+let musicaFondo
+let sonido = true
+
+
+function preload() {
+  // Cargar imágenes con los nombres correctos
+  fondoJuego = loadImage("./images/fondoJuego.jpg")
+  fondoInicio = loadImage("./images/fondoInicio.jpg")
+  pared = loadImage("./images/pared.png")
+  jaeminBird = loadImage("./images/jaeminBird.png")
+  
+  // Cargar sonidos
+  musicaRecord = loadSound("./sounds/aplauso.wav")
+  musicaFondo = loadSound("./sounds/musicaFondo.mp3")
+}
+
+function setup() {
+  createCanvas(800, 600)
+  noCursor()
+}
+
 function checkCollision() {
   // Tamaño del pájaro
   let birdWidth = 80
@@ -7,22 +42,19 @@ function checkCollision() {
   // Verificar colisión con cada par de tubos
   for (let i = 0; i < wallX.length; i++) {
     // Ancho aproximado del tubo
-    let tuboWidth = pared.width
+    let tuboWidth = 100
     
-    // Verificar si el pájaro está en el rango horizontal del tubo
+ 
     if (posX + birdWidth/2 > wallX[i] - tuboWidth/2 && 
         posX - birdWidth/2 < wallX[i] + tuboWidth/2) {
       
-      // Altura del espacio entre tubos (ajusta este valor según tu juego)
-      let espacioEntreParedes = 200
+    
+      let espacioSuperior = wallY[i] - 150
+      let espacioInferior = wallY[i] + 150
       
-      // Calcular límites de los tubos
-      let tuboSuperiorBottom = wallY[i] - 500 + pared.height/2
-      let tuboInferiorTop = wallY[i] + 500 - pared.height/2
-      
-      // Verificar colisión con tubo superior o inferior
-      if (posY - birdHeight/2 < tuboSuperiorBottom || 
-          posY + birdHeight/2 > tuboInferiorTop) {
+     
+      if (posY - birdHeight/2 < espacioSuperior || 
+          posY + birdHeight/2 > espacioInferior) {
         return true
       }
     }
@@ -31,14 +63,13 @@ function checkCollision() {
   return false
 }
 
-
 function draw() {
   if (estado === 1) {
-    // JUEGO EN MARCHA
+    
     background(255)
     imageMode(CORNER)
     
-    // Fondo en movimiento (escalado al tamaño del canvas)
+    
     image(fondoJuego, x, 0, width, height)
     image(fondoJuego, x + width, 0, width, height)
     x = x - 5
@@ -55,7 +86,7 @@ function draw() {
       image(pared, wallX[i], wallY[i] - 500)
       image(pared, wallX[i], wallY[i] + 500)
       
-      // Nuevos obstáculos
+    
       if (wallX[i] < -pared.width) {
         wallX[i] = width + 100
         wallY[i] = random(200, 300)
@@ -70,14 +101,13 @@ function draw() {
       wallX[i] = wallX[i] - 5
     }
     
-    // **AGREGAR ESTA VALIDACIÓN DE COLISIÓN**
     // Validar colisiones con tubos y bordes
     if (posY < -60 || posY > height + 60 || checkCollision()) {
       musicaFondo.stop()
       estado = 0
     }
 
-    // Despliega personaje (Jaemin Bird)
+    
     imageMode(CENTER)
     image(jaeminBird, posX, posY, 80, 80)
     
@@ -90,27 +120,27 @@ function draw() {
     text("Puntaje: " + puntaje, width / 2, 40)
     
   } else if (estado === 0) {
-    // PANTALLA DE INICIO / GAME OVER
+    
     imageMode(CORNER)
     image(fondoInicio, 0, 0, width, height)
     
     cursor()
     
-    // Textos en la parte derecha (lado negro)
+   
     textAlign(CENTER)
     
-   
+    // Puntaje máximo 
     textSize(40)
     fill(255)
     stroke(0)
     strokeWeight(3)
     text("Puntaje Máximo: " + puntajeMax, width * 0.75, 110)
     
-    
+    // Instrucción para jugar
     textSize(32)
     text("Presiona para iniciar", width * 0.75, 240)
     
-    
+    // Reproducir sonido de record
     if (puntajeMax > recordAnterior) {
       if (!musicaRecord.isPlaying() && sonido) {
         musicaRecord.play()
@@ -118,4 +148,31 @@ function draw() {
       }
     }
   }
+}
+
+function mousePressed() {
+  if (estado === 0) {
+    // Iniciar juego
+    estado = 1
+    x = 0
+    posX = 100
+    posY = 100
+    dY = 3
+    wallX = [500, 800, 1100]
+    wallY[0] = random(200, 300)
+    wallY[1] = random(200, 300)
+    wallY[2] = random(200, 300)
+    puntaje = 0
+    sonido = true
+    recordAnterior = puntajeMax
+    
+    if (musicaRecord.isPlaying()) {
+      musicaRecord.stop()
+    }
+    musicaFondo.loop()
+    noCursor()
+  }
+  
+  // Hacer que el pájaro salte
+  dY = -15
 }
